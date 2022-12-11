d84e6c25707ac8f064cad3e99e4eaa64227cfa87
Fran Mastucci <franmastucci@gmail.com>
2022-12-11 00:54:06 -0300

    some details

--- request/request/src/main/java/com/example/request/service/impl/ExampleRequestServiceImpl.java
+++ request/request/src/main/java/com/example/request/service/impl/ExampleRequestServiceImpl.java
@@ -31,23 +31,21 @@ public class ExampleRequestServiceImpl implements ExampleRequestService {
 	@Value("${restTemplate.serverPath}")
 	private String API_RESPONSE_SERVER_PATH;
 	
+	private RestTemplate restTemplate = new RestTemplate();
 	
+	private HttpHeaders headers = new HttpHeaders();
 	
-
-	// Da un error 405 "metodo no permitido". 
-	// TODO : se debería reemplazar el Método getoForEntity()
-	// 		  por el famoso método "exchange()"
 	
 	public ResponseEntity<CVUResponse> saveFromResponseApi(String token, CVURequest request)  {
-		RestTemplate restTemplate = new RestTemplate();
-		HttpHeaders headers = new HttpHeaders();
+
 		headers.set("Content-Type", MediaType.APPLICATION_JSON_VALUE);
 		headers.set("Accept", MediaType.APPLICATION_JSON_VALUE);
 		headers.add("Authorization", token);
-        HttpEntity<CVURequest> entity = new HttpEntity<CVURequest>(request, headers);
-        ResponseEntity<CVUResponse> response = restTemplate.exchange("http://localhost:8081/response/cvu", HttpMethod.POST, entity, CVUResponse.class);
-        System.out.println("Result - status ("+ response.getStatusCode() + ") has body: " + response.hasBody());
 		
+        HttpEntity<CVURequest> entity = new HttpEntity<CVURequest>(request, headers);
+        ResponseEntity<CVUResponse> response = restTemplate.exchange(API_RESPONSE_SERVER_PATH, HttpMethod.POST, entity, CVUResponse.class);
+        
+        System.out.println("Result - status ("+ response.getStatusCode() + ") has body: " + response.hasBody());
 		
         return response;
 	}
