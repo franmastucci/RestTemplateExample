e22efce36732951d388cb865cb6140da749cc0e2
Fran Mastucci <franmastucci@gmail.com>
2022-12-11 13:21:29 -0300

    unused imports has been deleted

--- request/src/main/java/com/example/request/config/client/CVUClient.java
+++ request/src/main/java/com/example/request/config/client/CVUClient.java
@@ -1,7 +1,6 @@
 package com.example.request.config.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
