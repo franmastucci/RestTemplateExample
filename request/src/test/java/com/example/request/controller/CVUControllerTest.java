0b8a3552fd0f3c1833508969993b6a8fe5101782
Fran Mastucci <franmastucci@gmail.com>
2022-12-11 19:04:37 -0300

    unit test for CVUCOntroller on request side

---
+++ request/src/test/java/com/example/request/controller/CVUControllerTest.java
@@ -0,0 +1,53 @@
+package com.example.request.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.example.request.model.dto.CVURequest;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class CVUControllerTest {
+	
+    @Autowired
+    public MockMvc mockMvc;
+    
+    @Autowired
+    protected ObjectMapper objectMapper;
+	
+    private static final String CREATE_CVU = "/request/cvu";
+
+    @Test
+    public void should_return_200ok_whit_a_valid_cvu_request() throws Exception {
+        mockMvc.perform(post(CREATE_CVU)
+        				.header(HttpHeaders.AUTHORIZATION, "Bearer token from test Header")
+                        .content(this.buildRequest())
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is2xxSuccessful());
+    }
+
+	private String buildRequest() throws JsonProcessingException {
+		String request = objectMapper.writeValueAsString(CVURequest.builder()
+    			.pspId(987654321L)
+    			.ctaId(12345678L)
+    			.cvu("1111111")
+    			.tipo("Cuenta Virtual Unificada")
+    			.cuit("2022333449")
+    			.titular("Juan Perez")
+    			.moneda("ARS")
+    			.personaTipo("F")
+    			.build());
+		return request;
+	}
+	
+}
